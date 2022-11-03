80042b76176924cbbf5843ff0e6cd739c665d4e8
IceIsNotADev <iceisnotadev@gmail.com>
2022-11-03 17:55:13 -0500

    hhhhhhhhhhh

--- pages/api/intruder.js
+++ pages/api/intruder.js
@@ -6,12 +6,12 @@ export default function intruder(req, res) {
     const {locat} = req.headers;
     if (req.query.usagekey === process.env.special_key && req.method === "POST") {
         console.log(locat);
-        fs.writeFile('public/location.json', `{"location" : ${locat.toString()}}`, function (err){return err});
-        res.status(200).json({message: "Location updated"});
+        fs.writeFile('public/location.json', `{"location" : ${locat.toString()}}`, function (err){return res.status(200).json({message: "failed"})});
+        return res.status(200).json({message: "Location updated"});
     }
     if (req.method === "GET") {
-        res.status(200).json(location.location);
+        return res.status(200).json(location.location);
     } else {
-        res.status(404).json({message: "You cannot perform this action on this endpoint"});
+        return res.status(404).json({message: "You cannot perform this action on this endpoint"});
     }
 }

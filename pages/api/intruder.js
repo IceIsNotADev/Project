00c2e7e8892e7eba2a9823571de612611e81c072
IceIsNotADev <iceisnotadev@gmail.com>
2022-11-03 18:58:02 -0500

    hi!

--- pages/api/intruder.js
+++ pages/api/intruder.js
@@ -1,23 +1,12 @@
 import location from "../../public/location.json";
+
 const fs = require('fs');
-import PocketBase from 'pocketbase';
 
-const client = new PocketBase('http://127.0.0.1:8090');
-let last = "h";
-let current = 0;
-
-
-export default async function intruder(req, res) {
+export default function intruder(req, res) {
     const {locat} = req.headers;
     if (req.query.usagekey === process.env.special_key && req.method === "POST") {
-        if(last !== "h"){
-            last = current;
-            await client.records.delete('current', last);
-        }
-        current = await client.records.create('current', locat);
-        fs.writeFileSync('public/location.json', `{"location" : ${locat.toString()}}`, function (err) {
-            return res.status(400).json({message: "failed"})
-        });
+        console.log(locat);
+        fs.writeFileSync('public/location.json', `{"location" : ${locat.toString()}}`, function (err){return res.status(400).json({error: err})});
         return res.status(200).json({message: "Location updated"});
     }
     if (req.method === "GET") {

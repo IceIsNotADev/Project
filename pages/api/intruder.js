ea75eed05e49f4d0cbba57c8fecd1bf9e6c9ea98
IceIsNotADev <iceisnotadev@gmail.com>
2022-11-03 16:36:05 -0500

    hiii

---
+++ pages/api/intruder.js
@@ -0,0 +1,17 @@
+import location from "../../public/location.json";
+
+const fs = require('fs');
+
+export default function intruder(req, res) {
+    if (req.method === "POST") {
+        fs.writeFile('public/location.json', `{"location" : ${req.query.il.toString()}}`, (err) => {
+            res.status(400).json({message: "An error has occurred while trying to update the location."})
+        })
+        res.status(200).json({message: "Location updated"});
+    }
+    if (req.method === "GET") {
+        res.status(200).json(location.location);
+    } else {
+        res.status(404).json({message: "You cannot perform this action on this endpoint"});
+    }
+}
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function index() {
+    return (
+        <>
+            <div>
+                <h1>Random Use API</h1>
+            </div>
+        </>
+    )
+}

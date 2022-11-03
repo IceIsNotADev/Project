f69189d04185655b011c4bea5b61a6275dce6993
IceIsNotADev <iceisnotadev@gmail.com>
2022-11-03 17:27:13 -0500

    hiiii

--- pages/api/intruder.js
+++ pages/api/intruder.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 export default function intruder(req, res) {
     if (req.query.usagekey === process.env.special_key && req.method === "POST") {
-        fs.writeFile('public/location.json', `{"location" : ${req.body}}`, (err) => {
+        fs.writeFile('public/location.json', `{"location" : ${req.body.locat}}`, (err) => {
             res.status(400).json({message: "An error has occurred while trying to update the location."})
         })
         res.status(200).json({message: "Location updated"});

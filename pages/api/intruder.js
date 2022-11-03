795464f0d3394c81ae5aaf6d9d2157cfb84fa62a
IceIsNotADev <iceisnotadev@gmail.com>
2022-11-03 17:08:28 -0500

    fixxxx

--- pages/api/intruder.js
+++ pages/api/intruder.js
@@ -1,20 +1,19 @@
 import location from "../../public/location.json";
+
 const fs = require('fs');
 
 export default function intruder(req, res) {
-    if(req.query.usagekey === process.env.special_key) {
-        if (req.method === "POST") {
-            fs.writeFile('public/location.json', `{"location" : ${req.body.toString()}}`, (err) => {
-                res.status(400).json({message: "An error has occurred while trying to update the location."})
-            })
-            res.status(200).json({message: "Location updated"});
-        }
-        if (req.method === "GET") {
-            res.status(200).json(location.location);
-        } else {
-            res.status(404).json({message: "You cannot perform this action on this endpoint"});
-        }
+    if (req.query.usagekey === process.env.special_key && req.method === "POST") {
+        fs.writeFile('public/location.json', `{"location" : ${req.body.toString()}}`, (err) => {
+            res.status(400).json({message: "An error has occurred while trying to update the location."})
+        })
+        res.status(200).json({message: "Location updated"});
     } else {
         res.status(400).json({message: "You do not have permission to perform this action. Provided key: " + req.body.usagekey});
+    }
+    if (req.method === "GET") {
+        res.status(200).json(location.location);
+    } else {
+        res.status(404).json({message: "You cannot perform this action on this endpoint"});
     }
 }

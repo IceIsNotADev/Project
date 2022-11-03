baa9300dba95993514f603ed1932e797e42248fd
IceIsNotADev <iceisnotadev@gmail.com>
2022-11-03 18:13:34 -0500

    h

--- pages/api/intruder.js
+++ pages/api/intruder.js
@@ -1,12 +1,11 @@
 import location from "../../public/location.json";
-
 const fs = require('fs');
 
 export default function intruder(req, res) {
     const {locat} = req.headers;
     if (req.query.usagekey === process.env.special_key && req.method === "POST") {
         console.log(locat);
-        fs.writeFile('public/location.json', `{"location" : ${locat.toString()}}`, function (err){return res.status(200).json({message: "failed"})});
+        fs.writeFileSync('public/location.json', `{"location" : ${locat.toString()}}`, function (err){return res.status(400).json({message: "failed"})});
         return res.status(200).json({message: "Location updated"});
     }
     if (req.method === "GET") {

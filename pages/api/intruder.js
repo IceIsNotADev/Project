0c620d1056793abf23f82c045f3e65a712ff84c1
IceIsNotADev <iceisnotadev@gmail.com>
2022-11-03 16:52:18 -0500

    Updated

--- pages/api/intruder.js
+++ pages/api/intruder.js
@@ -2,9 +2,9 @@ import location from "../../public/location.json";
 const fs = require('fs');
 
 export default function intruder(req, res) {
-    if(req.query.usagekey === process.env.special_key) {
+    if(req.body.usagekey === process.env.special_key) {
         if (req.method === "POST") {
-            fs.writeFile('public/location.json', `{"location" : ${req.query.il.toString()}}`, (err) => {
+            fs.writeFile('public/location.json', `{"location" : ${req.body.il.toString()}}`, (err) => {
                 res.status(400).json({message: "An error has occurred while trying to update the location."})
             })
             res.status(200).json({message: "Location updated"});
